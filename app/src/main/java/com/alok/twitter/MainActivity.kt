c9ca58a8b4c8b25944a129449de7290d7ec3b80f
Mishra, Alok | Alok | TDD <alok.mishra@rakuten.com>
2022-07-07 22:13:20 +0900

    add splash Screen

--- app/src/main/java/com/alok/twitter/MainActivity.kt
+++ app/src/main/java/com/alok/twitter/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.alok.twitter.ui.MainScreen
 import com.alok.twitter.ui.theme.TwitterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             TwitterTheme {
                 Surface() {
